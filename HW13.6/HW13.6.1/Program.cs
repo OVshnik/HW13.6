06e757bfb070015a2c61692bdebb83c958c87fda
Svyatoslav Petrov <sp.gro.33@gmail.com>
2024-03-04 13:39:21 +0300

    Add tasks

---
+++ HW13.6/HW13.6.1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Security.Cryptography;
+using HW13_6_1;
+using System.Diagnostics;
+using System.Windows.Markup;
+
+
+namespace HW13_6_1
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        var path = Path.Combine("C:\\Users\\MSI\\Documents", "Text.txt");
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        Console.WriteLine("Add file in List");
+        for (int i = 0; i < 10; i++)
+        {
+            timer.Restart();
+            WriteInList(path);
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed.Milliseconds);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Add file in LinkedList");
+        for (int i = 0; i < 10; i++)
+        {
+            timer.Restart();
+            WriteInLinkedList(path);
+            timer.Stop();
+            Console.WriteLine(timer.Elapsed.Milliseconds);
+        }
+    }
+    static void WriteInLinkedList(string path)
+    {
+
+        LinkedList<string> values = new();
+
+        string? symbols = File.ReadAllText(path);
+
+        var array = symbols.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var item in array)
+        {
+            values.AddFirst(item);
+        }
+
+
+    }
+    static void WriteInList(string path)
+    {
+        List<string> values = new();
+
+        string? symbols = File.ReadAllText(path);
+
+        var array = symbols.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (var item in array)
+        {
+            values.Add(item);
+        }
+
+
+    }
+}
+}
---
+++ HW13.6/HW13.6.2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Security.Cryptography;
+using HW13_6_2;
+using System.Diagnostics;
+using System.Windows.Markup;
+
+namespace HW13_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path1 = Path.Combine("C:\\Users\\MSI\\Documents", "Text.txt");
+
+            List<string> values = new();
+
+            string? symbols = File.ReadAllText(path1);
+            var array = symbols.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in array)
+            {
+                values.Add(item);
+            }
+
+            var uniqWords = values.ToHashSet();
+            Dictionary<string, int> keyValuePairs = new();
+            foreach (string word in uniqWords)
+            {
+                keyValuePairs.Add(word, 0);
+            }
+
+            foreach (var i in values)
+            {
+                foreach (var pair in keyValuePairs)
+                {
+                    if (i == pair.Key)
+                    {
+                        keyValuePairs[pair.Key] = pair.Value + 1;
+                    }
+                }
+            }
+
+            var result = keyValuePairs.OrderByDescending(z => z.Value);
+            int j = 0;
+            Console.WriteLine("10 наиболее распространенных слов в тексте:");
+            for (var i = result.ElementAt(j); j < 10; j++)
+            {
+                Console.WriteLine($" слово ({result.ElementAt(j).Key}) используется {result.ElementAt(j).Value} раз");
+            }
+        }
+    }
+}
